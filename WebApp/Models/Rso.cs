92b1e42e7ded3fe025f4dfa58448b143357aead4
yshikita <yendyjc@gmail.com>
2017-04-18 05:23:55 -0400

    changes in some views

--- WebApp/Models/Rso.cs
+++ WebApp/Models/Rso.cs
@@ -9,6 +9,16 @@ namespace WebApp.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }       
+        public string Description { get; set; }
+    }
+
+    public class WebRso
+    {
+        public string Name { get; set; }
+        public string AdName { get; set; }
+        public string AdEmail { get; set; }
+        public int NumMemb { get; set; }
+        public string Memb { get; set; }
+        public string Descr { get; set; }
     }
 }
