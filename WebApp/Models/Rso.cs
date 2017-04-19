b45020da49f5ea1d91a1fea8e7656084ec9475df
yshikita <yendyjc@gmail.com>
2017-04-19 02:08:03 -0400

    fixed build errors.

--- WebApp/Models/Rso.cs
+++ WebApp/Models/Rso.cs
@@ -10,6 +10,7 @@ namespace WebApp.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public int UniversityId { get; set; }
     }
 
     public class WebRso
